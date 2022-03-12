3f113f6350e89a3f970364ebd14868921a9d14e1
Oliver Harrison <oliverharrison255@gmail.com>
2022-03-11 16:22:10 -0800

    made the login for admin

---
+++ client/login.js
@@ -0,0 +1,28 @@
+const usernameInput = document.getElementById("usernameInput");
+const passwordInput = document.getElementById("passwordInput");
+const responseText = document.getElementById("responseText");
+const xhttp = new XMLHttpRequest();
+const endPoint ="https://SERVER_NAME";
+
+xhttp.onreadystatechange = function () {
+    if (xhttp.readyState == 4) {
+        if (xhttp.status == 200) {
+            responseText.innerHTML = `<div>${xhttp.response}</div>`;
+        } else if (xhttp.status == 404) {
+            responseText.innerHTML = "Invalid Credentials";
+        }
+    }
+};
+
+let logIn = function () {
+    let username = usernameInput.value;
+    let password = passwordInput.value;
+    if (!username.trim().length || !password.trim().length) {
+        alert('Cannot leave any fields blank');
+        return;
+    }
+
+    xhttp.open("POST", endPoint, true);
+    xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhttp.send("?username=" + username + "&password=" + password);
+};
