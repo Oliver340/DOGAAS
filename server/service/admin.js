7ad41d2b012db2dbf01576c59535199840a21d84
Patrick Ng <png85@my.bcit.ca>
2022-04-03 21:13:22 -0700

    Fixed admin login

--- server/service/admin.js
+++ server/service/admin.js
@@ -1,3 +1,5 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const connection = require('../utils/databaseConnection');
 const dbUtil = require('../utils/databaseUtil');
 
@@ -10,8 +12,8 @@ module.exports = {
 
         // login admin
         // Input validation
-        connection.query(`SELECT 1 FROM Admins 
-                          WHERE password = '${password}' AND adminName = '${username}'`, 
+        connection.query(`SELECT password FROM Admins 
+                          WHERE adminName = '${username}'`, 
         (sqlErr, sqlRes) => {
             if (sqlErr) {
                 res.status(500).send(JSON.stringify({ message: "Database error!" }));
@@ -20,29 +22,33 @@ module.exports = {
             if (Object.keys(sqlRes).length === 0) {
                 res.status(401).send(JSON.stringify({ message: "Invalid username or password" }));
             } else {
-                const token = jwt.sign(
-                    { 
-                        username: username,
-                    },
-                    tokenKey,
-                    {
-                        expiresIn: "12h"
-                    }
-                );
-                
-                // Provide endpoint data
-                connection.query(`SELECT * FROM EndPoints`, (sqlErr2, sqlRes2) => {
-                    if (sqlErr2) {
-                        res.status(500).send(JSON.stringify({ message: "Database error!" }));
-                    }
-                    
-                    if (sqlRes2[0]["1"] > 0) {
-                        res.status(401).send(JSON.stringify({ message: "Invalid username or password" }));
+                bcrypt.compare(password, sqlRes[0]["password"], function(err, result) {
+                    if (result == true) {
+                        const token = jwt.sign(
+                            { 
+                                username: username,
+                            },
+                            tokenKey,
+                            {
+                                expiresIn: "12h"
+                            }
+                        );
+                        
+                        // Provide endpoint data
+                        connection.query(`SELECT * FROM EndPoints`, (sqlErr2, sqlRes2) => {
+                            if (sqlErr2) {
+                                res.status(500).send(JSON.stringify({ message: "Database error!" }));
+                            }
+                            
+                             // Increment end point usage counter
+                             dbUtil.incrementEndPoint('/API/v1/adminPost');
+        
+                             res.status(200).send(JSON.stringify({ token: token, endpoints: sqlRes2 }));
+                        });
                     } else {
-                        // Increment end point usage counter
                         dbUtil.incrementEndPoint('/API/v1/adminPost');
 
-                        res.status(200).send(JSON.stringify({ token: token, endpoints: sqlRes2 }));
+                        res.status(401).send(JSON.stringify({ message: "Invalid username or password!" }));
                     }
                 });
             }
