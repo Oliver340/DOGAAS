153656d5cd284746f204a07f5a796297ce334617
Oliver Harrison <oliverharrison255@gmail.com>
2022-04-03 17:14:38 -0700

    fixed things

--- client/getDog.js
+++ client/getDog.js
@@ -1,14 +1,18 @@
 const dogImg = document.getElementById("dogImg");
 const dogTag = document.getElementById("dogTag");
 const responseText = document.getElementById("responseText");
+const editForm = document.getElementById("editDogForm");
 const xhttp = new XMLHttpRequest();
 const endPoint = "https://dogaas.patrickng.ca/"; //CHANGE
+
+let dogID;
 
 xhttp.onreadystatechange = function() {
     if (xhttp.readyState == 4) {
         if (xhttp.status == 200) {
             let jsonData = JSON.parse(xhttp.response);
-            dogImg.src = jsonData[0];
+            dogImg.src = jsonData[0].imageURL;
+            dogID = jsonData[0].dogId;
         } else if (xhttp.status == 500) {
             let jsonData = JSON.parse(xhttp.response);
             responseText.innerHTML = `<div>${jsonData.message}</div>`;
@@ -25,8 +29,25 @@ const getDog = function() {
         xhttp.open("GET", endPoint + "/api/v1/dog", true);
         xhttp.send();
     } else {
-        xhttp.open("GET", endPoint + "/api/v1/dog", true);
+        xhttp.open("GET", endPoint + "/api/v1/dog" + selectedOption, true);
         xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-        xhttp.send("?tagID=" + selectedOption);
+        xhttp.send();
     }
+}();
+
+let deleteDog = function () {
+    xhttp.open("DELETE", endPoint + '/api/v1/dog' + dogID, true);
+    xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhttp.send();
 };
+
+editForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    editUrl(editForm.urlInput.value);
+})
+
+const editURL = function(newURL) {
+    xhttp.open("PUT", endPoint + '/api/v1/dog' + dogID, true);
+    xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhttp.send("?imageURL=" + newURL);
+}
